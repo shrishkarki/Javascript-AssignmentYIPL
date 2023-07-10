f37f19bb11e703c36849e52d8897e977a5d43bf3
unknown <shrishkarki6669@gmail.com>
2023-07-10 17:16:48 +0545

    added different Functionalies

---
+++ Calculator/calculator.js
@@ -0,0 +1,43 @@
+const buttons = document.querySelectorAll('li');
+const inputvalue = document.querySelector(".calculator__value");
+const resultEvaluate = document.getElementById('result');
+const clearInput = document.getElementById('clear');
+const remove = document.getElementById('oneItemDelete');
+let FinalResult;
+
+
+const contentValue = (e) => {
+    if (e.target === resultEvaluate) {
+        FinalResult = eval(inputvalue.value);
+
+        inputvalue.value = FinalResult;
+
+        return;
+
+
+    }
+
+    if (e.target === clearInput) {
+        inputvalue.value = "";
+
+        return;
+    }
+
+    if (e.target === remove) {
+
+        let arr = Object.assign([], inputvalue.value);
+        arr.pop();
+        let str = arr.join("");
+        inputvalue.value = str;
+
+        return;
+    }
+
+    inputvalue.value += e.target.innerText;
+}
+
+buttons.forEach((item) => {
+
+
+    item.addEventListener('click', contentValue);
+})
